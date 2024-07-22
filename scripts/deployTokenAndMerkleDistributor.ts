4f45a4a2b673fef1f66be407970f41504ef3b951
Antoine Estienne <estienne.antoine@gmail.com>
2024-07-22 12:42:37 +0200

    use local merkleroot

--- scripts/deployTokenAndMerkleDistributor.ts
+++ scripts/deployTokenAndMerkleDistributor.ts
@@ -1,8 +1,29 @@
+import * as fs from 'fs'
+
 require('dotenv').config()
-require('@nomiclabs/hardhat-ethers')
-const { ethers } = require('hardhat')
+import '@nomiclabs/hardhat-ethers'
+import { ethers } from 'hardhat'
 
 const DISTRIBUTOR_INIT_BALANCE = 1000000
+
+interface MerkleData {
+  merkleRoot: string
+}
+
+export const getLocalMerkleRoot = () => {
+  // Read the JSON file
+  const data = fs.readFileSync('./outputFiles/merkle.json', 'utf-8')
+
+  // Parse the JSON data
+  const jsonData: MerkleData = JSON.parse(data)
+
+  // Extract the merkleRoot and assign it to the constant root
+  const root = jsonData.merkleRoot
+
+  // Log the result (or use it in your application)
+  console.log('Merkle Root:', root)
+  return root
+}
 
 async function main() {
   // Deploy example token
@@ -15,7 +36,7 @@ async function main() {
   const MerkleDistributor = await ethers.getContractFactory('MerkleDistributor')
   const merkleDistributor = await MerkleDistributor.deploy(
     token.address,
-    process.env.DEPLOY_ROOT,
+    process.env.DEPLOY_ROOT === 'local' ? getLocalMerkleRoot() : process.env.DEPLOY_ROOT,
     process.env.TX_SIGNER_ADDRESS
   )
   await merkleDistributor.deployed()
